f4e8e5eda7505c0d92162ea96521d3c5b6fa3a25
GCaringi <giannicaringi94@gmail.com>
2022-05-04 11:45:54 +0200

    snack-2

---
+++ snack-2/main.js
@@ -0,0 +1,8 @@
+let userStr = prompt("Inserisci una parola")
+let userStrCmp = prompt("Inserisci una parola")
+
+if (userStr.length >= userStrCmp.length){
+    console.log(userStr, userStrCmp);
+}else{
+    console.log(userStrCmp, userStr);
+}
