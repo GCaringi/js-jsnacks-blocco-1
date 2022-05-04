e8975314aca315e77576947227e924f96c1a4719
GCaringi <giannicaringi94@gmail.com>
2022-05-04 16:56:36 +0200

    add more name and surname in lists

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,8 +1,8 @@
 
 // Generatore di "nomi cognomi" casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
-const nameList = ["gianni", "marco", "giulio", "matteo"];
-const surnameList = ["rossi", "bianchi", "verdi", "pazzi"];
+const nameList = ["gianni", "marco", "giulio", "matteo, federico", "samuele", "eligio", "fabio"];
+const surnameList = ["rossi", "bianchi", "verdi", "pazzi", "sarra", "black", "red", "white"];
 const fakeList = [];
 
 for (let i = 0; i < nameList.length; i++){
