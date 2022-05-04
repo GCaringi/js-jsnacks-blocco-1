38543d2d6911f5fccf7bb22b079695258cb7e5aa
GCaringi <giannicaringi94@gmail.com>
2022-05-04 17:16:55 +0200

    Add template literal inside for

--- snack-7/main.js
+++ snack-7/main.js
@@ -1,12 +1,10 @@
-
-// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
-
-const nameList = ["gianni", "marco", "giulio", "matteo, federico", "samuele", "eligio", "fabio"];
+const nameList = ["gianni", "marco", "giulio", "matteo", "federico", "samuele", "eligio", "fabio"];
 const surnameList = ["rossi", "bianchi", "verdi", "pazzi", "sarra", "black", "red", "white"];
 const fakeList = [];
 
 for (let i = 0; i < nameList.length; i++){
-    fakeList.push(nameList[Math.floor(Math.random()*nameList.length)] + " " +  surnameList[Math.floor(Math.random()*surnameList.length)]);
+    //fakeList.push(nameList[Math.floor(Math.random()*nameList.length)] + " " +  surnameList[Math.floor(Math.random()*surnameList.length)]);
+    fakeList.push(`${nameList[Math.floor(Math.random()*nameList.length)]} ${surnameList[Math.floor(Math.random()*surnameList.length)]}`);
 }
 
 console.log(fakeList);
