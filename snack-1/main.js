6bb15dec418165ac252667a0891d5cfcbe901018
GCaringi <giannicaringi94@gmail.com>
2022-05-04 11:32:27 +0200

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,10 @@
+let userNum = prompt("Inserisci un numero")
+let userNumCmp = prompt("Inserisci un numero")
+
+if (userNum > userNumCmp){
+    alert(userNum)
+}else if (userNum < userNumCmp){
+    alert(userNumCmp)
+}else{
+    alert("I numeri sono uguali");
+}
